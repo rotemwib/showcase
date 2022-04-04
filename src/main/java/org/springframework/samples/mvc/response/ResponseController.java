4452e50838f109a91207f9bfaf84602283cd1a71
Rotem Mizrachi <rotem@wib.com>
2022-04-04 14:19:36 +0300

    first commit

---
+++ src/main/java/org/springframework/samples/mvc/response/ResponseController.java
@@ -0,0 +1,59 @@
+package org.springframework.samples.mvc.response;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.samples.mvc.convert.JavaBean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/response", method=RequestMethod.GET)
+public class ResponseController {
+
+	@GetMapping("/annotation")
+	public String responseBody() {
+		return "The String ResponseBody";
+	}
+
+	@GetMapping("/charset/accept")
+	public String responseAcceptHeaderCharset() {
+		return "\u3053\u3093\u306b\u3061\u306f\u4e16\u754c\uff01 (\"Hello world!\" in Japanese)";
+	}
+
+	@GetMapping(value="/charset/produce", produces="text/plain;charset=UTF-8")
+	public String responseProducesConditionCharset() {
+		return "\u3053\u3093\u306b\u3061\u306f\u4e16\u754c\uff01 (\"Hello world!\" in Japanese)";
+	}
+
+	@GetMapping("/entity/status")
+	public ResponseEntity<String> responseEntityStatusCode() {
+		return new ResponseEntity<String>("The String ResponseBody with custom status code (403 Forbidden)",
+				HttpStatus.FORBIDDEN);
+	}
+
+	@GetMapping("/entity/headers")
+	public ResponseEntity<String> responseEntityCustomHeaders() {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.TEXT_PLAIN);
+		return new ResponseEntity<String>("The String ResponseBody with custom header Content-Type=text/plain",
+				headers, HttpStatus.OK);
+	}
+
+	@GetMapping("/entity/bla")
+	public ResponseEntity<JavaBean> responseEntityBla() {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		if (headers.containsKey("das")) {
+			return ResponseEntity.notFound().build();
+		} else if (headers.containsKey("sadas")) {
+			return ResponseEntity.status(202).body(new JavaBean());
+		}
+		ResponseEntity<JavaBean> b = ResponseEntity.ok(new JavaBean());
+		return b;
+	}
+
+}
